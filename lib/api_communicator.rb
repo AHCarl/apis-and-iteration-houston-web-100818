5d2b188ef7363abb456d12e2f28d8e61a7400e1d
AHCarl <ahcarlin@gmail.com>
2018-10-09 19:26:35 -0500

    not pretty, but it works

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,32 +6,27 @@ def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
 
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  #find where downcased input matches downcased character in [results][index]
+  result = response_hash["results"].find { |n| n["name"].downcase == character.downcase }
+  #set films urls from that index to a variable
+  film_result = result["films"]
+  #parse those urls for JSON and collect the titles
+  film_info = film_result.collect {|i| JSON.parse(RestClient.get(i))}
+  #return the titles
+  film_info
 end
 
-def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+#iterate over hash and put titles with index
+def parse_character_movies(films_hash)
+  films_hash.each.with_index(1) do |data, index| 
+  puts "#{index} " + data['title'] 
+  end
 end
 
+#return titles and pass to parse as arg method
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
-  print_movies(films_array)
+  parse_character_movies(films_array)
 end
 
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
